750e2a21e29b97ce002dc8fc8bd5b4060068df8b
Jan Kenith Johannessen <jankenith.johannessen@gmail.com>
2018-09-22 14:34:22 +0800

    Added initial class

---
+++ Assets/Game/Scripts/Interface/ICharacter.cs
@@ -0,0 +1,17 @@
+﻿using Game.Scripts.Model;
+
+namespace Assets.Game.Scripts.Interface
+{
+    public interface ICharacter
+    {
+        int CurrentHP();
+        void DeductHP(int dmg = 1);
+        EnumState CurrentState();
+        void SetState(EnumState state);
+        IItem CurrentItem();
+        void ReceiveItem(IItem item);
+        void ThrowItem();
+        void Skill();
+    }
+
+}
